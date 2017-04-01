5cb988ffdd7e737fb1dd3aaafe4320b4adafd6ee
Charles Reid <charlesreid1@gmail.com>
2017-04-01 09:05:32 +0000

    adding stunnel to startup install task

--- install_task.sh
+++ install_task.sh
@@ -10,14 +10,17 @@ export PYB="/usr/bin/python"
 export SCR="/root/codes/pi-aircrack-batch/AircrackIntervals.py"
 
 export LINE="${PYB} ${SCR}"
+export LINE2="/usr/bin/stunnel /etc/stunnel/stunnel.conf"
 
 echo "------"
-echo "Adding the following line to /etc/rc.local:"
+echo "Adding the following lines to /etc/rc.local:"
 echo "${LINE}"
+echo "${LINE2}"
 
 # This should only create a .bak if it finds a result.
 sed -i.bak "s'exit 0''" /etc/rc.local
 echo "${LINE}" >> /etc/rc.local
+echo "${LINE2}" >> /etc/rc.local
 
 echo "------"
 echo "New /etc/rc.local:"
