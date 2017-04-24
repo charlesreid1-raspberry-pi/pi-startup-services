d4e8eaf4debfc6ad13c2a23d76504262b07ab5a9
Charles Reid <charlesreid1@gmail.com>
2017-04-24 09:32:34 +0000

    modify pi install task - exclude stunnel.

--- install_task.sh
+++ install_task.sh
@@ -10,22 +10,23 @@ export PYB="/usr/bin/python"
 export SCR="/root/codes/pi-aircrack-batch/AircrackIntervals.py"
 
 export LINE="${PYB} ${SCR}"
-export LINE2="/usr/bin/stunnel /etc/stunnel/stunnel.conf"
+#export LINE2="/usr/bin/stunnel /etc/stunnel/stunnel.conf"
 
 echo "------"
 echo "Adding the following lines to /etc/rc.local:"
 echo "${LINE}"
-echo "${LINE2}"
+#echo "${LINE2}"
 echo "------"
 
 # This should only create a .bak if it finds a result.
 sed -i.bak "s'exit 0.*''" /etc/rc.local
 
 # check if the lines are already there
-for L in "${LINE}" "${LINE2}"
+#for L in "${LINE}" "${LINE2}"
+for L in "${LINE}" 
 do
 	# if the line is already there, don't add it.
-	echo "Is line already there? Count:"
+	echo "Checking if the line we are adding to rc.local is already there."
 	echo "$(grep -c "${L}" /etc/rc.local)"
 	if [[ "$(grep -c "${L}" /etc/rc.local)" == "0" ]]; then
 		echo "${L}" >> /etc/rc.local
